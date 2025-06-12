ffa29131070c0ff0b912ff389ded1bcc18d027d5
advika sharmna <sharmaadvikaa@gmail.com>
2025-06-13 00:13:47 +0530

    working with files

--- python_week2.py
+++ python_week2.py
@@ -141,3 +141,56 @@ class Private:
 obj = Private()
 print(obj.salary())  # Works
 #print(obj.__salary)  # Raises AttributeError
+
+#python opeaning and reading files
+#File Handling
+#The key function for working with files in Python is the open() function.
+#The open() function takes two parameters; filename, and mode.
+#There are four different methods (modes) for opening a file:
+#"r" - Read - Default value. Opens a file for reading, error if the file does not exist
+#
+#"a" - Append - Opens a file for appending, creates the file if it does not exist
+#
+#"w" - Write - Opens a file for writing, creates the file if it does not exist
+#
+#"x" - Create - Creates the specified file, returns an error if the file exists
+#In addition you can specify if the file should be handled as binary or text mode
+#
+#"t" - Text - Default value. Text mode
+#
+#"b" - Binary - Binary mode (e.g. images)
+
+f=open("demofile.txt")
+print(f.read())
+f.close()
+
+#or
+with open("demofile.txt") as f:
+  print(f.read(5))
+  print(f.readline())
+  print(f.readline())
+  
+#looping
+with open("demofile.txt") as f:
+  for x in f:
+    print(x)
+
+#write to file
+with open("demofile.txt", "a") as f:
+  f.write("Now the file has more content!")
+
+#open and read the file after the appending:
+with open("demofile.txt") as f:
+  print(f.read())
+  
+#overwrite the file
+with open("demofile.txt", "w") as f:
+  f.write("Woops! I have deleted the content!")
+
+#open and read the file after the overwriting:
+with open("demofile.txt") as f:
+  print(f.read())
+  
+#delete a file
+import os
+os.remove("demofile.txt")
